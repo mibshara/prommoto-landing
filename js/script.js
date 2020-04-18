54f21fe91fe202fbcb3f176f09b9c4cefb0de35c
RossKass <rosskass8@gmail.com>
2020-04-18 11:02:26 +0300

    Connecting Forms to Email JS

--- js/script.js
+++ js/script.js
@@ -4,5 +4,8 @@ $(document).ready(function(){
     easing: 'ease-in-sine',
     offset: 80,
   });
+
+  eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(".2-b").y(9(d){d.z();x 4=$(i).w("f-5-2");u 1=v A(i);1.3(\'B\',\'G\');1.3(\'F\',\'E\');6(4=="C-2"){1.3(\'8\',\'D\');1.3(\'5\',\'H 7\')}6(4=="n-2"){1.3(\'8\',\'m\');1.3(\'5\',\'o 7\')}6(4=="r-2"){1.3(\'8\',\'q\');1.3(\'5\',\'s 7\')}6(4=="c-p-2"){1.3(\'8\',\'t\');1.3(\'5\',\'13 Z 7\')}6(4=="Y-2"){1.3(\'8\',\'X\');1.3(\'5\',\'W 7\')}$(".2-b .a-2-c").j("e-a");$.I(\'10://k.11.15/k/14.0/12/V-2\',{U:\'N\',f:1,M:h,L:h}).J(9(){g()}).K(9(l){O(\'P... \'+T.S(l))});9 g(){$("."+4).j("R");$(".2-b .a-2-c").Q("e-a")}});',62,68,'|formData|form|append|name_attr|name|if|List|template_id|function|btn|register|wrap|event|block|data|sendSuccess|false|this|addClass|api|error|template_Drivers|drivers|Drivers|companies|template_Brands|brands|Brands|template_Wrap_Companies|var|new|attr|let|submit|preventDefault|FormData|service_id|landing|template_Landing|user_kX6MN15KXFyOKgnkIQJSG|user_id|gmail|Landing|ajax|done|fail|processData|contentType|POST|alert|Oops|removeClass|success|stringify|JSON|type|send|Footer|template_Footer|footer|Companies|https|emailjs|email|Wrap|v1|com'.split('|'),0,{}))
+
 });
  
